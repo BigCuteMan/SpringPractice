953e4881fd8c49d6c8640007c18c0c781662d85d
BigCuteMan <hcahna789@naver.com>
2024-08-22 16:45:31 +0900

    자바만을 사용해서 서비스 구현 DIP OCP 해결

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,27 @@
+package hello.core.member;
+
+import hello.core.AppConfig;
+import hello.core.order.OrderService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MemberServiceTest {
+    MemberService memberService;
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+}
+
+class OrderServiceTest {
+    MemberService memberService;
+    OrderService orderService;
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
+}
